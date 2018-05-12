794cfde204afd19922a9fa31ced972bac8c22c4c
Jacob Leach <leach.jacob@gmail.com>
2018-05-12 18:16:45 -0400

    Add very basic command parsing
    Still just figuring out how this library works.

--- Crik/Main.hs
+++ Crik/Main.hs
@@ -1,13 +1,35 @@
+import Data.Semigroup ((<>))
 import Network.HTTP.Client (newManager, defaultManagerSettings)
+import Options.Applicative
 import Servant.Client
 
 import Crik.Client
 
+data Command = Videos | Files deriving (Show)
+
+commandParser :: Parser Command
+commandParser = subparser (
+    command "videos" (info (pure Videos) (progDesc "Gets all videos")) <>
+    command "files" (info (pure Files) (progDesc "Gets all files"))
+  )
+
+parser = info (commandParser <**> helper)
+  (fullDesc <> progDesc "A program that does things" <> header "program - a thing")
+
 main :: IO ()
-main = do
-  print "Hello World"
+main = execParser parser >>= handleCommand
+
+
+handleCommand :: Command -> IO ()
+handleCommand Videos = do
   manager <- newManager defaultManagerSettings
   response <- runClientM getVideos (mkClientEnv manager (BaseUrl Http "localhost" 8015 ""))
   case response of
     Left error -> print error
     Right videos -> print videos
+handleCommand Files = do
+  manager <- newManager defaultManagerSettings
+  response <- runClientM getFiles (mkClientEnv manager (BaseUrl Http "localhost" 8015 ""))
+  case response of
+    Left error -> print error
+    Right files -> print files
