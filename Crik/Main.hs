f8fb3cbf94e63109440080d67313ecd0b66ce794
Jacob Leach <leach.jacob@gmail.com>
2018-05-12 20:50:16 -0400

    Add libraries and print out as JSON

--- Crik/Main.hs
+++ Crik/Main.hs
@@ -1,3 +1,5 @@
+import Data.Aeson (ToJSON, encode)
+import qualified Data.ByteString.Lazy as BS (putStr)
 import Data.Semigroup ((<>))
 import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Options.Applicative
@@ -5,31 +7,35 @@ import Servant.Client
 
 import Crik.Client
 
-data Command = Videos | Files deriving (Show)
+data Command = Videos | Files | Libraries deriving (Show)
 
 commandParser :: Parser Command
 commandParser = subparser (
     command "videos" (info (pure Videos) (progDesc "Gets all videos")) <>
-    command "files" (info (pure Files) (progDesc "Gets all files"))
+    command "files" (info (pure Files) (progDesc "Gets all files")) <>
+    command "libraries" (info (pure Libraries) (progDesc "Gets all files"))
   )
 
 parser = info (commandParser <**> helper)
   (fullDesc <> progDesc "A program that does things" <> header "program - a thing")
 
+getAPIEnvironment :: IO (ClientEnv)
+getAPIEnvironment = do
+  manager <- newManager defaultManagerSettings
+  return (mkClientEnv manager (BaseUrl Http "localhost" 8015 ""))
+
 main :: IO ()
-main = execParser parser >>= handleCommand
+main = do
+  apiEnvironment <- getAPIEnvironment
+  execParser parser >>= handleCommand apiEnvironment
 
+handleCommand :: ClientEnv -> Command -> IO ()
+handleCommand environment Videos = runClientM getVideos environment >>= handleResponse
+handleCommand environment Files = runClientM getFiles environment >>= handleResponse
+handleCommand environment Libraries = runClientM getLibraries environment >>= handleResponse
 
-handleCommand :: Command -> IO ()
-handleCommand Videos = do
-  manager <- newManager defaultManagerSettings
-  response <- runClientM getVideos (mkClientEnv manager (BaseUrl Http "localhost" 8015 ""))
-  case response of
-    Left error -> print error
-    Right videos -> print videos
-handleCommand Files = do
-  manager <- newManager defaultManagerSettings
-  response <- runClientM getFiles (mkClientEnv manager (BaseUrl Http "localhost" 8015 ""))
-  case response of
-    Left error -> print error
-    Right files -> print files
+handleResponse :: ToJSON a => Either ServantError a -> IO ()
+handleResponse (Left error) = print error
+handleResponse (Right x) = do
+  BS.putStr (encode x)
+  putStrLn ""
